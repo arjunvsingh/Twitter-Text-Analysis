0cfed5d37f5f8d9a5f2c016b20b8635ae372836e
Singh, Arjun Vivek <ajspsp@Arjun.local>
2020-08-05 20:53:02 -0400

    Upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ISyE 6740 Final Project Summer 2020',
+    author='Arjun Singh',
+    license='MIT',
+)
